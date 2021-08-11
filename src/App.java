4679efe587eee68f45795cf0164aa7cce384f774
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2021-08-11 11:48:43 -0300

    Created custom project

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        String wiewsPath = "./info/views/";
+
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(wiewsPath + "CustomView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("JavaFX Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
